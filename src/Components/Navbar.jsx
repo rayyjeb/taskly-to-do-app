e73fa4108bc0856219603104314300be07520ee0
Rajab Shoukath Kunnath <rajabshoukathis@gmail.com>
2024-10-30 16:00:06 +0530

    Styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
   return (
+    // Navbar Styling
     <div className="Navbar">
       <div className="LogoDiv">
         <img className="Logo" src={Logo} />
